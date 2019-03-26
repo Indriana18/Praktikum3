405bec11c595009f7f788250a2b9ce3a7462c6b9
Indriana18 <48426491+Indriana18@users.noreply.github.com>
2019-03-26 14:53:15 +0700

    Add files via upload

---
+++ LatihanMemberClass/LatihanMemberClass/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    public class Taxi
+    {
+        //properties
+        public string DriveName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drive Name : {0}", DriveName);
+            Console.WriteLine("On Duty : {0} ", OnDuty ? "Yes" : "No");
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger ()
+
+        {
+            Console.WriteLine("\n sedang menjemput Penumpang ", DriveName);
+        }
+        public void DropOffPassenger()
+
+        {
+            Console.WriteLine("\n selesai mengantar Penumpang ", DriveName);
+        }
+
+    }
+}
